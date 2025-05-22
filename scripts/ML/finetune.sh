c583a054d787df23073f0864055683d273743b78
Adrian Tissera <consultant-atissera@remine.com>
2025-05-23 02:09:46 +0400

    Added finetune script

--- scripts/ML/finetune.sh
+++ scripts/ML/finetune.sh
@@ -14,7 +14,7 @@ mlx_lm.lora \
     --model $MODEL \
     --train \
     --data $ROOT_PATH/data/training/nl2SQL \
-    --adapter-path $ROOT_PATH/models/Llama-3.2-3B-Instruct-4bit/nl2SQL/adapters \
+    --adapter-path $ROOT_PATH/experiments/models/Llama-3.2-3B-Instruct-4bit/nl2SQL/adapters \
     --iters 500 \
     --batch-size 2 \
     --num-layers 16
